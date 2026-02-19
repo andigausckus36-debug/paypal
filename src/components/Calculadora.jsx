0224dc9e0aa4eb6a94a7acc1b8aef7cefb223206
andigausckus36-debug <andigausckus36@gmail.com>
2026-02-19 11:52:50 +0000

    actualización

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -47,8 +47,8 @@ const applyPayPalFee = (grossUSD) => {
 Spreads
 ----------------------- */
 // Spreads generales
-const SPREAD_COMPRAR = 0.09;
-const SPREAD_VENDER = 0.10;
+const SPREAD_COMPRAR = 0.07;
+const SPREAD_VENDER = 0.08;
 
 // Tipos de cambio MXN
 const MXN_RATE_VENDER = 16.00;
